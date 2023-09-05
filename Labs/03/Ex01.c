08fbb5347fb6e3696f9af4d047ecb82f834dd3d1
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-06 00:20:38 +0500

    Update Ex01.c

--- Labs/03/Ex01.c
+++ Labs/03/Ex01.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+main() {
+	int x, y;
+	char D;
+	x=0;
+	y=0;
+
+	printf("this is a calulator....please enter the two numbers x and y \n");
+	printf("x=");
+	scanf("%d", &x);
+	printf("y=");
+	scanf("%d", &y);
+	printf("Please select symbol a for addition s subraction d for division m for multiplication \n");
+	scanf(" %c", &D);
+	 if (D == 'a') { 
+        printf("%d\n", x + y);
+    } else if (D == 's') {
+        printf("%d\n", x - y);
+    } else if (D == 'm') { 
+        printf("%d\n", x * y);
+    } else if (D == 'd') {
+	printf("%f\n", x/y );
+	}
+    	
+
+}
 
