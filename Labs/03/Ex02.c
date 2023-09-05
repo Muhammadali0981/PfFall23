dbc0d4a5ed30641ce46a4c64e1570517a6f469fd
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-05 10:25:13 +0500

    Update Ex02.c

--- Labs/03/Ex02.c
+++ Labs/03/Ex02.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+main() {
+	int a, b, c;
+	a=0;
+	b=0;
+	c=0;
+	printf("this is a program to calculate volume....please enter the three sides a,b and c \n");
+	printf("a=");
+	scanf("%d", &a);
+	printf("b=");
+	scanf("%d", &b);
+	printf("c=");
+	scanf("%d", &c);
+	printf("volume is= %d", a*b*c);
+	
+}// end main()
