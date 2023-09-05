48a53174ed2209129b8296c6aa718a3723a2da32
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-06 00:31:52 +0500

    Create Ex05.c

---
+++ Labs/03/Ex05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+main() {
+	int DT;
+	DT=0;
+	printf ("please enter the distnace travelled \n");
+	scanf("%d", &DT);
+	printf ("your fuel used is=%d", DT*2 );
+
+	
+
+}
+
+	
+// end main()
