093b24c743565a58c32fa035eff7f6a36c863747
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-05 10:39:06 +0500

    Update Ex03.c

--- Labs/03/Ex03.c
+++ Labs/03/Ex03.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
 
+main() {
+	int opp, adj;
+	opp=0;
+	adj=0;
+	printf("this is a program to calculate hypothenous...please enter the two sides adj,opp \n");
+	printf("adj=");
+	scanf("%d", &adj);
+	printf("opp=");
+	scanf("%d", &opp);
+	printf("hypothenous is= %f", sqrt((opp*opp)+(adj*adj)));
+	
+}// end main()
