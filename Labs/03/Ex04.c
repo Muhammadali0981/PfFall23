85bf9be2631bfa1fe51e932d642d19a406d666ee
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-05 10:59:15 +0500

    Update Ex04.c

--- Labs/03/Ex04.c
+++ Labs/03/Ex04.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+main() {
+	int price, discount;
+	float discountValue;
+	price=0;
+	discount=0;
+	discountValue=0;
+	printf("this is a program to calculate sale price given the distcount and original...please enter the distcount and original\n");
+	printf("price=");
+	scanf("%d", &price);
+	printf("discount=");
+	scanf("%d", &discount);
+	discountValue= price*(1 - ((float)discount/100));
+	printf("sale price is= %f",  discountValue);
+	
+}// end main()
