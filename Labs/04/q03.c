24e072802e0be15f3267e62eceebf7184bd2b6d8
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-12 22:10:13 +0500

    Update q03.c

--- Labs/04/q03.c
+++ Labs/04/q03.c
@@ -1,16 +1,22 @@
+/*
+* Programmer:Muhammad Ali 
+* Date: 12/09/23
+* Descrption:inputs a number n and prints the sum of their squares 
+		
+*/
 #include <stdio.h>
 
 int main(){
-	int n, num, total;
+	int n, num, total; //n is the number of inputs whike num is the number being inputed total is the final output
 	total=0;
 	printf ("enter the amount of numbers you want to input=");
 	scanf ("%d",&n);
-	while (n>0){
+	while (n>0){ //loop to take n inputs
 		num=0;
 		printf("enter number=");
 		scanf("%d",&num);
-		total=total+(num*num);
-		n=n-1;
+		total=total+(num*num); //squaring and adding
+		n=n-1; //decrementing
 	}
 	printf("%d",total);
 }
