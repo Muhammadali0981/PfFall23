3d8ae409b9e23377fa4591c5e8a8622364b03af7
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-19 09:03:03 +0500

    Create q1.c

---
+++ Labs/05/q1.c
@@ -0,0 +1,19 @@
+/*
+* Programmer:Muhammad Ali 
+* Date: 19/09/23
+* Descrption:To check for multiple of 3
+		
+*/
+#include <stdio.h>
+int main(){
+	int n;
+	printf("enter any number=");
+	scanf("%d",&n);
+	if(n%3==0){//simply checking remainder by using mod
+		printf("number is multiple of 3");
+	}
+	else{
+		printf("number is not a multiple of 3");
+	}
+		
+		
