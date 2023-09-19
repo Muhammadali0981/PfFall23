222ecd8eaee481e217bb4dea0a4a83f266356cc5
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-19 09:23:54 +0500

    Create q02.c

---
+++ Labs/05/q02.c
@@ -0,0 +1,22 @@
+/*
+* Programmer:Muhammad Ali 
+* Date: 19/09/23
+* Descrption:To check for time value and give prompt accordingly
+		
+*/
+#include <stdio.h>
+int main(){
+	int time;
+	printf("enter current time=");
+	scanf("%d",&time);
+	if((time>=5) && (time<=11)){
+		printf("Good Morning");
+	}
+	else if((time>=12) && (time<=18)){
+		printf("Good Evening");
+	}
+	else if((time>=18) && (time<=24)){
+		printf("Good Night");
+	}
+		
+}	
