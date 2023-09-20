fe40be2814d113e100e2e237db9fb5a4cce8d73b
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-09-20 14:40:35 +0500

    Create q09.c

---
+++ Labs/05/q09.c
@@ -0,0 +1,34 @@
+/*
+* Programmer:Muhammad Ali 23K-0052
+* Date: 20/09/23
+* Descrption:sloves the quadratic equation using the qaudartic equations shows when solutions are not real  
+		
+*/
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a, b, c;
+	float x1, x2, d; //floats as the values can be decimal
+	printf("kindly follow this pattern while inputting\nax^2+bx+c a b and c are to be taken as inputs \n");
+	printf("enter a="); //taking iputs
+	scanf("%d", &a);
+	printf("enter b=");
+	scanf("%d", &b);
+	printf("enter c=");
+	scanf("%d", &c);
+	d=(b*b)-4*(a)*(c);//discriminant 
+	if (d<0){//considering all possibilitiles 
+		printf("no real solutions");
+	}else if (d>0){
+		printf("two solutions exist ");
+		x1=(((-b)+(float)sqrt(d))/(2*a));
+		x2=(((-b)-(float)sqrt(d))/(2*a));
+		printf("x1=%f",x1);
+		printf(", x2=%f",x2);
+	}else if (d==0){
+		printf("only one solution exists ");
+		x1=(-b/2*a);
+		printf("x1=%f",x1);
+	}
+
+}
