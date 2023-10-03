b1e96e139f788eba9207267bab4357caea9adc96
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-03 21:19:06 +0500

    Update q05.c

--- Labs/06/q05.c
+++ Labs/06/q05.c
@@ -1,11 +1,16 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 03/10/23
+* Descrption:number pattern printing
+*/
+#incl
 #include <stdio.h>
 
 int main() {
-    int a[6];
+    int a[7];
     int i=0;
-    while (i<6){
-        a[i]=i+1;
-        if (a[i]%2==0){
+    while (i<7){
+        a[i]=i;
+        if (a[i]%2==0){//checking for odd an even
             printf("\n%d \t%d \t%d \t%d" ,a[i],a[i],a[i],a[i]);
         }
         else{
