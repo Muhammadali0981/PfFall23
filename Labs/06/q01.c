0ef8eb5bef71b5800b69be3273fb8cc981cd1fe9
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-03 08:42:24 +0500

    Create q01.c

---
+++ Labs/06/q01.c
@@ -0,0 +1,28 @@
+/*
+* Programmer:Muhammad Ali 23K-0052
+* Date: 03/10/23
+* Descrption:tells who is older between two people 
+		
+*/
+#include <stdio.h>
+
+int main(){
+	int n,i, total=0;
+	printf("please input a number:\n");
+	scanf("%d", &n);
+	i=1;
+	while (i<n){
+		if(n%i==0){
+			total += i;
+		}
+		i++;
+	
+	}
+	if(total==n){
+		printf("number is perfect");
+	}
+	else{
+		printf("number is not prefect");
+	}
+	
+}
