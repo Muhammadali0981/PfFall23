44b9bb10efcd1e9df6f467c4a703f53ceb0d7b7c
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-03 21:57:24 +0500

    Create q07.c

---
+++ Labs/06/q07.c
@@ -0,0 +1,26 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 03/10/23
+* Descrption:number pattern printing
+*/
+#include <stdio.h>
+int main(){
+	int a[100];
+	int s=0,n;
+	printf("please input size of array=");
+	scanf("%d", &n);
+  //validating array lenght 
+	if(n<=0 || n>101){
+		printf("please in put a valid input");
+	}
+	else{
+    //inputting values and summing them 
+		while (n>0){
+			printf("input value ");
+			scanf("%d", &a[n]);
+			s+=a[n];
+			n--;
+	    }
+	    printf("%d", s);
+	}
+
+}
