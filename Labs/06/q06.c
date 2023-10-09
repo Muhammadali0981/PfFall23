b55a2295effd4b62ece5f9a0effe4c36ea272101
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-09 15:09:56 +0500

    Create q06.c

---
+++ Labs/06/q06.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int main() {
+	int sticks = 21, c;
+	printf("Sticks %d\n", sticks);
+
+	while (sticks > 0) {
+		
+		do {
+			printf("Enter number of sticks 1, 2, 3 or 4: ");
+			scanf("%d", &c);
+		} while (c > 4 || c < 1);
+		sticks -= c;
+
+		if (sticks > 1) {
+			switch (c) {//by using this logic the computer always wins 
+				case 4:
+					printf("computer picked 1 stick\n");
+					sticks -= 1;
+					break;
+				case 3:
+					printf("computer picked 2 sticks\n");
+					sticks -= 2;
+					break;
+				case 2:
+					printf("computer picked 3 sticks\n");
+					sticks -= 3;
+					break;
+				case 1:
+					printf("computer picked 4 sticks\n");
+					sticks -= 4;
+					break;
+			}
+		}
+		
+
+		if (sticks >= 0) 
+			printf("Sticks left %d\n", sticks);
+		else 
+			printf("Sticks left 0");
+	}
+
+	return 0;
+}
