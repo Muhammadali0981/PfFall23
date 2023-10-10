f43ba282163fbad1d888850dab595054a7be2c98
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-10 08:41:29 +0500

    Create q01.c

---
+++ Labs/07/q01.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 10/10/23
+* Descrption:division without divide sign
+*/
+
+
+
+int main(){
+	int a,b,c;
+	printf("use this format for input a/b mae sure to write / in between");
+	printf("\nenter a/b=");
+	scanf("%d/%d",&a,&b );
+	while (a>0){//conunting the number of times b can be subracted from a
+		if(a<b){
+			break;
+		}
+		a-=b;
+		c++;
+	}
+	printf("Answer is %d", c);
+return 0;
+}
