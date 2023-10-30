0d6df4b3f1bf853dc04d54e63362346d554ede48
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-30 15:12:14 +0500

    Update q03.c

--- Assignments/Assignment02/q03.c
+++ Assignments/Assignment02/q03.c
@@ -1,3 +1,8 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 29/10/23
+* Descrption:flight manager
+*/
+
 #include<stdio.h>
 
 int main(){
@@ -16,41 +21,53 @@ int main(){
     scanf("%d",&Day);
     int j=0;
     
-    printf("what time do you prefer for flying select e for evening m for morning");
+    printf("what time do you prefer for flying select e for evening m for morning:");
     scanf(" %c", &TimeChoice);
+    int i;
     switch(TimeChoice){
         case 'e':
         
-            for(int i = 0; i < 5; i++){
+            for(i = 0; i < 5; i++){
                 if(a[i][4]<MinPrice){
                     MinPrice=a[i][4];
                     MinPriceDay=a[i][0];
                 }
             }
+        break;
         
         case 'm':
           
-            for(int i = 0; i < 5; i++){
+            for(i = 0; i < 5; i++){
                 if(a[i][2]<MinPrice){
                     MinPrice=a[i][4];
                     MinPriceDay=a[i][0];
                 }
             }  
-                 
+        break;         
     }
     
     //outputs (should have really made sperate functions for this)
     if(a[Day-1][j+1]==0 && a[Day-1][j+3]==1){
-        printf("flight available in evening costing %d", a[Day-1][j+4]);
-        }
+        printf("flight available in evening on slected day costing %d", a[Day-1][j+4]);
+    }
+        
     else if(a[Day-1][j+3]==0 && a[Day-1][j+1]==1){
-        printf("flight available in morning costing %d", a[Day-1][j+2]);
+        printf("flight available in morning on slected day costing %d", a[Day-1][j+2]);
+    }
+    
+    else if (a[Day-1][j+3]==1 && a[Day-1][j+1]==1){
+    	printf("flights available in both morning and evening on slected day costing %d in the morning and %d in the evening ", a[Day-1][j+2], a[Day-1][j+4]);	
+	}   
+	
+    else{
+        printf("no flights available on selected day");
         }
         
-    else{
-        printf("no flights available on this day");
-        }
-    if(TimeChoice==m){
-        printf("");
+       
+    if(TimeChoice=='m'){
+        printf("\ncheapest flight in the morning is on day %d costing %d", MinPriceDay, MinPrice);
+    }
+    else if(TimeChoice=='e'){
+        printf("\ncheapest flight in the evening is on day %d costing %d", MinPriceDay, MinPrice);
     }
 }
