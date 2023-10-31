72ccdaf60d9c09d3c6dd69b5973c369980e8f866
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-10-31 10:41:34 +0500

    Create q03.c

---
+++ Labs/09b/q03.c
@@ -0,0 +1,20 @@
+
+
+#include<stdio.h>
+
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else return gcd(b, a%b);
+
+}
+
+int main(){
+    int a,b;
+    printf("enter num1 num2:");
+    scanf("%d %d", &a, &b);
+
+
+    printf ("%d", gcd(a,b));
+}
