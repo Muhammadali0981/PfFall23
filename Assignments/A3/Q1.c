c7ec3eade288ac37191622049c56db04c1c65ffd
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-12-05 23:22:18 +0500

    Create Q1.c

---
+++ Assignments/A3/Q1.c
@@ -0,0 +1,18 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 22/11/23
+* Descrption:making company employee management system through structs
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct{ 
+
+	char Name[50];
+	char Role[50];
+	int Communication;
+	int Teamwork;
+	int Creativity;
+
+}data;
