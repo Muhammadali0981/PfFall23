e21b7183c0f93f4932c9238941232ee86a871462
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2023-12-06 11:30:45 +0500

    Add files via upload

---
+++ Assignments/A3/Q1.c
@@ -0,0 +1,87 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 05/12/23
+* Descrption:making company employee management system through structs
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	int max=-10000;
+
+	if (argc != 2)
+	{
+		printf("please enter file name\n");
+		return 0;
+	}
+
+
+	FILE *fp=fopen(argv[1],"r");
+
+	printf("------------------------\nName:Muhammad Ali\nRoll Name:23K-0052\n------------------------\n");
+
+	printf("enter dimesion of square matrix even value:");
+	scanf("%d", &n);
+
+	int mat[n][n];
+	int sub[n/2][n/2];
+	int subrow=0,subcol =0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			fscanf(fp,"%d,",&mat[i][j]);
+			
+		}
+
+	}
+
+	for (int i = 0; i < n ; i+=2)
+	{
+		subcol=0;
+
+		for(int j = 0; j < n; j+=2)
+		{
+			max=-10000;
+			for (int row = i; row < i+2; ++row)
+			{
+				for (int col = j; col < j+2; ++col)
+				{
+					if(mat[row][col]>max){
+
+						max=mat[row][col];
+
+					}
+				}
+			}
+			sub[subrow][subcol]=max;
+		subcol++;
+		}
+	subrow++;
+	}
+
+	for (int i = 0; i < n/2; ++i)
+	{
+		for (int j = 0; j < n/2; ++j)
+		{
+			printf("%d ", sub[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
